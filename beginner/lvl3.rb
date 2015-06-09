5bd8aa831f0491632ba2323631f26daaee66e1b1
Ashley Watkins <AWatkins31@gmail.com>
2015-06-08 22:14:48 -0700

    Completed 6/8

---
+++ beginner/lvl3.rb
@@ -0,0 +1,37 @@
+class Player
+  def play_turn(warrior)
+    if warrior.feel.empty?
+      if warrior.health < 10
+        warrior.rest!
+      else
+        warrior.walk!
+      end
+    else
+      warrior.attack!
+    end
+  end
+end
+
+
+##### OR, IF I WANT TO BE COMPLICATED #####
+class Player
+  def play_turn(warrior)
+    if warrior.feel.empty?
+      rest_or_walk(warrior)
+    else
+      warrior.attack!
+    end
+  end
+
+  def rest_or_walk(warrior)
+    if enough_health?(warrior)
+      warrior.walk!
+    else
+      warrior.rest!
+    end
+  end
+
+  def enough_health?(warrior)
+    warrior.health > 10
+  end
+end
